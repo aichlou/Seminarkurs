05a69f784c1bc834f434a11108a6aaac9b5e937d
Lou <max@aicheler1.de>
2026-01-14 20:55:36 +0100

    Workaround for RPI.GPIO

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-import RPi.GPIO as GPIO
 import time
 import webserver
+
+try:
+    import RPi.GPIO as GPIO
+except (ImportError, RuntimeError):
+    class GPIO:
+        BOARD = "BOARD"
+        OUT = "OUT"
+        LOW = 0
+        HIGH = 1
+
+        @staticmethod
+        def setmode(mode):
+            print(f"[MOCK GPIO] setmode({mode})")
+
+        @staticmethod
+        def setup(pin, mode):
+            print(f"[MOCK GPIO] setup(pin={pin}, mode={mode})")
+
+        @staticmethod
+        def output(pin, state):
+            print(f"[MOCK GPIO] output(pin={pin}, state={state})")
 
 '''
 PUL = 11
@@ -17,13 +37,12 @@ GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(ENA, GPIO.OUT)
 
 GPIO.output(ENA, GPIO.LOW)
-
 GPIO.output(DIR, GPIO.HIGH)
 
 for _ in range(200000):
- GPIO.output(PUL, GPIO.HIGH)
- time.sleep(0.0001)
- GPIO.output(PUL, GPIO.LOW)
- time.sleep(0.0001)
+    GPIO.output(PUL, GPIO.HIGH)
+    time.sleep(0.0001)
+    GPIO.output(PUL, GPIO.LOW)
+    time.sleep(0.0001)
 
 GPIO.output(ENA, GPIO.HIGH)
