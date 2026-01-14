2f8ca8ffbdbb3fa197916fe21f90a8accc74a719
Lou <max@aicheler1.de>
2026-01-15 00:20:07 +0100

    Added new file and began to restructure

--- main.py
+++ main.py
@@ -1,48 +1,6 @@
 import time
+import threading
 import webserver
+import motor
 
-try:
-    import RPi.GPIO as GPIO
-except (ImportError, RuntimeError):
-    class GPIO:
-        BOARD = "BOARD"
-        OUT = "OUT"
-        LOW = 0
-        HIGH = 1
-
-        @staticmethod
-        def setmode(mode):
-            print(f"[MOCK GPIO] setmode({mode})")
-
-        @staticmethod
-        def setup(pin, mode):
-            print(f"[MOCK GPIO] setup(pin={pin}, mode={mode})")
-
-        @staticmethod
-        def output(pin, state):
-            print(f"[MOCK GPIO] output(pin={pin}, state={state})")
-
-'''
-PUL = 11
-DIR = 13
-ENA = 15 '''
-
-PUL = 8
-DIR = 10
-ENA = 12
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PUL, GPIO.OUT)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(ENA, GPIO.OUT)
-
-GPIO.output(ENA, GPIO.LOW)
-GPIO.output(DIR, GPIO.HIGH)
-
-for _ in range(200000):
-    GPIO.output(PUL, GPIO.HIGH)
-    time.sleep(0.0001)
-    GPIO.output(PUL, GPIO.LOW)
-    time.sleep(0.0001)
-
-GPIO.output(ENA, GPIO.HIGH)
+thread = threading.Thread(target=motor.rotate())
---
+++ motor.py
@@ -0,0 +1,55 @@
+import time
+try:
+    import RPi.GPIO as GPIO
+except (ImportError, RuntimeError):
+    class GPIO:
+        BOARD = "BOARD"
+        OUT = "OUT"
+        LOW = 0
+        HIGH = 1
+
+        @staticmethod
+        def setmode(mode):
+            print(f"[MOCK GPIO] setmode({mode})")
+
+        @staticmethod
+        def setup(pin, mode):
+            print(f"[MOCK GPIO] setup(pin={pin}, mode={mode})")
+
+        @staticmethod
+        def output(pin, state):
+            print(f"[MOCK GPIO] output(pin={pin}, state={state})")
+
+
+'''
+ '''
+
+
+def setup_motors():
+    GPIO.setmode(GPIO.BOARD)
+    for motor in range(2):
+        if motor == 0:
+            PUL = 8
+            DIR = 10
+            ENA = 12
+        elif motor == 1:
+            PUL = 11
+            DIR = 13
+            ENA = 15
+        GPIO.setup(PUL, GPIO.OUT)
+        GPIO.setup(DIR, GPIO.OUT)
+        GPIO.setup(ENA, GPIO.OUT)
+        GPIO.output(ENA, GPIO.LOW)
+        GPIO.output(DIR, GPIO.HIGH)
+
+for _ in range(200000):
+    GPIO.output(PUL, GPIO.HIGH)
+    time.sleep(0.0001)
+    GPIO.output(PUL, GPIO.LOW)
+    time.sleep(0.0001)
+
+GPIO.output(ENA, GPIO.HIGH)
+
+
+def rotate():
+    round
