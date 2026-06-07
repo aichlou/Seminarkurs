0b9db09f9628f1bfcd85b3382c1b89db88847a9a
Lou <max@aicheler1.de>
2026-06-07 18:29:18 +0200

    Init angefangen AHHHHH

--- RaspberryPi/init.py
+++ RaspberryPi/init.py
@@ -4,18 +4,27 @@ import sensor
 
 def init(sensordata, commands):
     motor.setup_motors()
-    commands.put(("start_motor", "X", 0.7))
-    start = time.time()
+   timedata = [0.0, 0.0]
     print("Starte x-Achsen Motor")
+    sensorcounter = 0
+    speed = 0.7
     while not sensordata.empty():
         sensordata.get()
-    while True:
+    while sensorcounter < 2:
+        commands.put(("start_motor", "X", speed))
         cmd = sensordata.get()
         pin, state = cmd
         print(f"Pin {pin} ist jetzt {state}")
         if pin in (0, 1) and state:
+            timedata[sensorcounter] = time.time()
             commands.put(("stop_motor", "X"))
-            time.sleep(3)
-            commands.put(("start_motor", "X", -0.7))
-            print("Stoppe Motor")
-            
+            speed = -speed
+            sensorcounter += 1
+
+    duration = timedata[1] - timedata[0]
+    
+    unit = duration / 5
+    
+    commands.put(("start_motor", "X", speed))
+    time.sleep(unit)
+    commands.put(("stop_motor", "X"))
